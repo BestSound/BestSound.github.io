0ff83245b39fc1864211456a2bdc7b940450ba4e
BestSound <74553062+BestSound@users.noreply.github.com>
2020-11-17 00:21:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,23 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Membuat Login Multi User Level Dengan PHP dan MySQLi - www.malasngoding.com</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
- 
-	<h1>Membuat Login Multi User Level Dengan PHP dan MySQLi <br/> www.malasngoding.com</h1>
+    	<? php
+      memerlukan_once "aplikasi / lib / koneksi.php";
+      membutuhkan_once "application / lib / config.php";
+      memerlukan_once "application / functions / app.php";
+      membutuhkan_once "views / header.php";
+      
+      $ pages_dir = 'halaman';
+      if (! empty ($ _ GET ['p'])) {
+        $ halaman = scandir ($ pages_dir, 0);
+        belum disetel ($ halaman [0], $ halaman [1]);
 
-	<?php 
-	if(isset($_GET['pesan'])){
-		if($_GET['pesan']=="gagal"){
-			echo "<div class='alert'>Username dan Password tidak sesuai !</div>";
-		}
-	}
-	?>
- 
-	<div class="kotak_login">
-		<p class="tulisan_login">Silahkan login</p>
- 
-		<form action="cek_login.php" method="post">
-			<label>Username</label>
-			<input type="text" name="username" class="form_login" placeholder="Username .." required="required">
- 
-			<label>Password</label>
-			<input type="password" name="password" class="form_login" placeholder="Password .." required="required">
- 
-			<input type="submit" class="tombol_login" value="LOGIN">
- 
-			<br/>
-			<br/>
-			<center>
-				<a class="link" href="https://www.malasngoding.com">kembali</a>
-			</center>
-		</form>
-		
-	</div>
- 
- 
-</body>
-</html>
+        $ p = $ _GET ['p'];
+        if (in_array ($ p. '. php', $ pages)) {
+         sertakan ($ pages_dir. '/'. $ p. '. php');
+       } lain {
+         echo 'Halaman tidak ditemukan! :( ';
+       }
+     } lain {
+      sertakan ($ pages_dir. '/ home.php');
+    }
+    membutuhkan_once "views / footer.php";
+    ?>
+
